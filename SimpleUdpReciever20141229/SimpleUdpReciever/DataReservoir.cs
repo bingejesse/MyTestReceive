c4d68922df636c67d0faa5416f4bf72a57685baa
jesse317 <bingejesse@163.com>
2015-03-04 13:53:29 +0800

    first

---
+++ SimpleUdpReciever20141229/SimpleUdpReciever/DataReservoir.cs
@@ -0,0 +1,132 @@
+﻿using System.Threading;
+using System;
+using System.IO;
+using System.Text;
+
+namespace SimpleUdpReciever
+{
+    class DataReservoir
+    {
+        private Thread persistenceThread;
+        private byte[] receiveBuffer = new byte[1024*1024*100];//缓存100MB
+        private int startIndex = 0;
+        private int endIndex = 0;
+        private FileStream fileStream;
+        private const string path = "d:/data.txt";
+
+        #region 构造函数
+        public DataReservoir()
+        {
+            this.InitFile();
+            this.persistenceThread = new Thread(new ThreadStart(ProcessingData));
+            this.persistenceThread.Start();
+        }
+        #endregion
+
+        private void InitFile()
+        {
+
+            //每次删除文件重新来
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            if (!File.Exists(path))
+            {
+                fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);//创建写入文件
+                fileStream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 添加数据入缓冲区
+        /// </summary>
+        /// <param name="getDataByte">待添加数据</param>
+        /// <param name="copyStartIndex">拷贝待添加数据起始位</param>
+        /// <param name="length">拷贝待添加数据长度</param>
+        /// <returns></returns>
+        public void AddProcessingDataByte(byte[] getDataByte, int copyStartIndex, int length)
+        {
+            try
+            {
+
+                int partLength = this.receiveBuffer.Length - this.endIndex - 1;//receiveBuffer.Length 最大值1024，endIndex最大值1023
+                //考虑如果超过缓冲的情况
+                if (length > partLength)
+                {
+                    this.AddProcessingDataByte(getDataByte, 0, partLength);
+                    this.AddProcessingDataByte(getDataByte, partLength, getDataByte.Length - partLength);
+                }
+                else
+                {
+                    Array.Copy(getDataByte, copyStartIndex, this.receiveBuffer, this.endIndex, length);
+                    this.endIndex += length;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("添加出错" + e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 处理数据
+        /// </summary>
+        /// <param name="getDataByte"></param>
+        private void ProcessingData()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                if (endIndex <= startIndex)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    /**
+                    FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write);
+                    //设定书写的开始位置为文件的末尾  
+                    fs.Position = fs.Length+1;
+                    string tempString = Encoding.Default.GetString(this.receiveBuffer, startIndex, endIndex - startIndex);
+
+                    BinaryWriter bw = new BinaryWriter(fs); //创建BinaryWriter对象
+                    //开始写入
+                    bw.Write(tempString);
+                    //清空缓冲区
+                    bw.Flush();
+                    //关闭流
+                    bw.Close();
+                    fs.Close();
+
+                    startIndex = 0;
+                    endIndex = 0;
+                     * 
+                     * */
+
+                    FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write);
+                    //设定书写的开始位置为文件的末尾  
+                    fs.Position = fs.Length + 1;
+                    fs.Write(this.receiveBuffer, startIndex, endIndex - startIndex);
+
+                    fs.Flush();
+                    fs.Close();
+                    startIndex = 0;
+                    endIndex = 0;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("处理数据"+e);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            fileStream.Close();
+        }
+
+    }
+}
---
+++ SimpleUdpReciever20141229/SimpleUdpReciever/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SimpleUdpReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int localPort = 11000;
+            IPEndPoint remoteSender = new IPEndPoint(IPAddress.Any, 0);
+            bool flag = false;
+
+            for( int i = 0; i < args.Length; i++)
+            {
+                string cmd = args[i];
+                string value;
+                int tempInt;
+                IPAddress tempAddress;
+                
+
+                switch (cmd)
+                {
+                    case "-lp":
+                        value = GetValue(args, ref i);
+                        if (int.TryParse(value, out tempInt))
+                            localPort = tempInt;
+                        break;
+                    case "-rp":
+                        value = GetValue(args, ref i);
+                        if (int.TryParse(value, out tempInt))
+                            remoteSender.Port = tempInt;
+                        break;
+                    case "-rh":
+                        value = GetValue(args, ref i);
+                        if (IPAddress.TryParse(value, out tempAddress))
+                            remoteSender.Address = tempAddress;
+                        else if (int.TryParse(value, out tempInt) && tempInt == 0)
+                            remoteSender.Address = IPAddress.Any;
+                        break;
+                    case "-?":
+                    default:
+                        PrintHelpText();
+                        flag = true;
+                        break;
+                }
+            }
+
+            // Exit application after help text is displayed
+            if (flag)
+                return;
+
+            // Display some information
+            Console.WriteLine("Welcome! Starting Upd receiving.");
+            Console.WriteLine("Local port: " + localPort);
+            Console.WriteLine("Remote ip: " + remoteSender.Address.ToString());
+            Console.WriteLine("Remote port: " + remoteSender.Port);
+            Console.WriteLine("Use '-?' to display help.");
+            Console.WriteLine("Press any key to quit.");
+            Console.WriteLine("*** Copyright Jesse 20141229 ***");
+            Console.WriteLine("*** Version 1.0.1 ***\n");
+
+            // Create UDP client
+            UdpClient client = new UdpClient(localPort);
+            UdpState state = new UdpState(client, remoteSender);
+            // Start async receiving
+            client.BeginReceive(new AsyncCallback(DataReceived), state);
+
+            // Wait for any key to terminate application
+            Console.ReadKey();
+            client.Close();
+        }
+
+        private static DataReservoir dataReservoir = new DataReservoir();
+
+        private static void DataReceived(IAsyncResult ar)
+        {
+            try
+            {
+                UdpClient c = (UdpClient)((UdpState)ar.AsyncState).c;
+                IPEndPoint wantedIpEndPoint = (IPEndPoint)((UdpState)(ar.AsyncState)).e;
+                IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+
+                dataReservoir.AddProcessingDataByte(receiveBytes, 0, receiveBytes.Length);
+
+                // Check sender
+                bool isRightHost = (wantedIpEndPoint.Address.Equals(receivedIpEndPoint.Address)) || wantedIpEndPoint.Address.Equals(IPAddress.Any);
+                bool isRightPort = (wantedIpEndPoint.Port == receivedIpEndPoint.Port) || wantedIpEndPoint.Port == 0;
+                if (isRightHost && isRightPort)
+                {
+                    // Convert data to ASCII and print in console
+                    //string receivedText = ASCIIEncoding.ASCII.GetString(receiveBytes);
+                    //Console.Write(receivedText);
+                }
+
+                // Restart listening for udp data packages
+                c.BeginReceive(new AsyncCallback(DataReceived), ar.AsyncState);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("DataReceived" + e);
+            }
+
+        }
+
+
+        private static string GetValue(string[] args, ref int i)
+        {
+            string value = String.Empty;
+            if (args.Length >= i + 2)
+            {
+                i++;
+                value = args[i];
+            }
+            return value;
+        }
+
+        private static void PrintHelpText()
+        {
+            Console.WriteLine("Simple Udp Receiver is an application that prints incoming data to screen.");
+            Console.WriteLine("Data is converted to ASCII before printing.");
+            Console.WriteLine("*** Copyright Jesse 20141229 ***");
+            Console.WriteLine("*** Version 1.0.1 ***\n");
+            Console.WriteLine("Command switches:");
+            Console.WriteLine("-? : Displays this text.");
+            Console.WriteLine("-lp : Set local receiving port. \"-lp 4001\" Default: 11000");
+            Console.WriteLine("-rp : Set remote sender port. \"-rp 4001\" Default: 0 (Any port)");
+            Console.WriteLine("-rh : Set remote sender ip. \"-rh 192.168.1.10\" Default: 0 (Any ip)");
+            Console.WriteLine("\n Example of usage:\nSimpleUdpReciver.exe -lp 11000 -rh 192.168.10.10 -rp 4001");
+        }
+    }
+}
